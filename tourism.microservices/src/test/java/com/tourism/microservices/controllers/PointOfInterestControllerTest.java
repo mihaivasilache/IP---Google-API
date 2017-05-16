65d6b7ece91ae6550cf5ce26b0fd4f7c3efb684f
iulianbute <iulian.bute@gmail.com>
2017-05-16 21:16:22 +0300

    am adaugat clase de testare

---
+++ tourism.microservices/src/test/java/com/tourism/microservices/controllers/PointOfInterestControllerTest.java
@@ -0,0 +1,25 @@
+package com.tourism.microservices.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Iulian on 16.05.2017.
+ */
+public class PointOfInterestControllerTest {
+    @Test
+    public void readUrl() throws Exception {
+        assertEquals(0, 0);
+    }
+
+    @Test
+    public void get() throws Exception {
+        assertEquals(4, 2);
+    }
+
+    @Test
+    public void getDetail() throws Exception {
+    }
+
+}
---
+++ tourism.microservices/src/test/java/com/tourism/microservices/models/PointOfInterestTest.java
@@ -0,0 +1,135 @@
+package com.tourism.microservices.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Iulian on 16.05.2017.
+ */
+public class PointOfInterestTest {
+    @Test
+    public void getLocation() throws Exception {
+    }
+
+    @Test
+    public void setId() throws Exception {
+    }
+
+    @Test
+    public void getId() throws Exception {
+    }
+
+    @Test
+    public void setLocation() throws Exception {
+    }
+
+    @Test
+    public void getName() throws Exception {
+    }
+
+    @Test
+    public void setName() throws Exception {
+    }
+
+    @Test
+    public void getDescription() throws Exception {
+    }
+
+    @Test
+    public void setDescription() throws Exception {
+    }
+
+    @Test
+    public void getAddress() throws Exception {
+    }
+
+    @Test
+    public void setAddress() throws Exception {
+    }
+
+    @Test
+    public void getIcon() throws Exception {
+    }
+
+    @Test
+    public void setIcon() throws Exception {
+    }
+
+    @Test
+    public void getTypes() throws Exception {
+    }
+
+    @Test
+    public void setTypes() throws Exception {
+    }
+
+    @Test
+    public void addType() throws Exception {
+    }
+
+    @Test
+    public void getLocationId() throws Exception {
+    }
+
+    @Test
+    public void setLocationId() throws Exception {
+    }
+
+    @Test
+    public void getFormattedNumber() throws Exception {
+    }
+
+    @Test
+    public void setFormattedNumber() throws Exception {
+    }
+
+    @Test
+    public void getRating() throws Exception {
+    }
+
+    @Test
+    public void setRating() throws Exception {
+    }
+
+    @Test
+    public void getReference() throws Exception {
+    }
+
+    @Test
+    public void setReference() throws Exception {
+    }
+
+    @Test
+    public void getUrl() throws Exception {
+    }
+
+    @Test
+    public void setUrl() throws Exception {
+    }
+
+    @Test
+    public void getVicinity() throws Exception {
+    }
+
+    @Test
+    public void setVicinity() throws Exception {
+    }
+
+    @Test
+    public void getWebsite() throws Exception {
+    }
+
+    @Test
+    public void setWebsite() throws Exception {
+    }
+
+    @Test
+    public void getReviews() throws Exception {
+    }
+
+    @Test
+    public void addReview() throws Exception {
+    }
+
+}
